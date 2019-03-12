2816aca420ffe1c747973ef64a25c9a9ed0b0b3f
Aleksey Burov <allburov@gmail.com>
2019-03-12 12:35:29 +0700

    done

--- Escher/the_ship_teams.py
+++ Escher/the_ship_teams.py
@@ -6,32 +6,33 @@
 # END_DESC
 
 def two_teams(sailors):
-    #replace this for solution
+    # replace this for solution
     return [
         [],
         []
     ]
 
+
 if __name__ == '__main__':
     print("Example:")
     print(two_teams({'Smith': 34, 'Wesson': 22, 'Coleman': 45, 'Abrahams': 19}))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for auto-testing
     assert two_teams({
-        'Smith': 34, 
-        'Wesson': 22, 
-        'Coleman': 45, 
+        'Smith': 34,
+        'Wesson': 22,
+        'Coleman': 45,
         'Abrahams': 19}) == [
-            ['Abrahams', 'Coleman'], 
-            ['Smith', 'Wesson']
-            ]
+               ['Abrahams', 'Coleman'],
+               ['Smith', 'Wesson']
+           ]
 
     assert two_teams({
         'Fernandes': 18,
         'Johnson': 22,
         'Kale': 41,
         'McCortney': 54}) == [
-            ['Fernandes', 'Kale', 'McCortney'], 
-            ['Johnson']
-            ]
+               ['Fernandes', 'Kale', 'McCortney'],
+               ['Johnson']
+           ]
     print("Coding complete? Click 'Check' to earn cool rewards!")
--- SendGrid/stressful_subject.py
+++ SendGrid/stressful_subject.py
@@ -2,17 +2,39 @@
 
 # https://py.checkio.org/mission/stressful-subject/
 
-# 
+import re
+
+#
 # END_DESC
+from itertools import groupby
+
 
 def is_stressful(subj):
     """
         recoognise stressful subject
     """
+    if subj.endswith("!!!"):
+        return True
+
+    if subj.upper() == subj:
+        return True
+
+    # "red" words
+    stress = ["help", "asap", "urgent"]
+    stress_chars = set("".join(stress))
+    words = subj.lower().split()
+    for word in words:
+        word = re.sub(f'[^{"".join(stress_chars)}]', '', word)
+        word = ''.join([k[0] for k in groupby(word)])
+        if word in stress:
+            return True
     return False
 
+
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for auto-testing
     assert is_stressful("Hi") == False, "First"
     assert is_stressful("I neeed HELP") == True, "Second"
+    assert is_stressful("I neeed HEeeLP") == True, "Second"
+    assert is_stressful("We need you AA.S.AA.P.!!") == True, "Second"
     print('Done! Go Check it!')
