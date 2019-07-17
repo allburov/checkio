3af4704622057105038e8ff3e4fc686a735106a1
Aleksey Burov <allburov@gmail.com>
2019-07-17 09:26:19 +0700

    the healers mission

--- Scientific Expedition/group_equal_consecutive.py
+++ Scientific Expedition/group_equal_consecutive.py
@@ -12,19 +12,9 @@
 # 
 # 
 # END_DESC
+from itertools import groupby
 
-def group_equal(els):
-    # your code here
+
+def group_equal(raw):
+    els = [list(g) for k, g in groupby(raw)]
     return els
-
-
-if __name__ == '__main__':
-    print("Example:")
-    print(group_equal([1, 1, 4, 4, 4, "hello", "hello", 4]))
-    
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert group_equal([1, 1, 4, 4, 4, "hello", "hello", 4]) == [[1,1],[4,4,4],["hello","hello"],[4]]
-    assert group_equal([1, 2, 3, 4]) == [[1], [2], [3], [4]]
-    assert group_equal([1]) == [[1]]
-    assert group_equal([]) == []
-    print("Coding complete? Click 'Check' to earn cool rewards!")
--- tests/test_mission_battle.py
+++ tests/test_mission_battle.py
@@ -1,4 +1,4 @@
-from Incinerator.the_lancers import *
+from IceBase.the_healers import *
 
 
 def test_lancer():
@@ -146,3 +146,130 @@ def test_from_task_13():
     army_2.add_units(Vampire, 13)
     battle = Battle()
     assert battle.fight(army_1, army_2) == True
+
+
+def test_army_list():
+    army_1 = Army()
+    v1 = Vampire()
+    v2 = Vampire()
+    v3 = Vampire()
+    army_1.append_units([v1])
+    army_1.append_units([v2])
+    army_1.append_units([v3])
+    army_1.add_units(Vampire, 10)
+    assert army_1.index(v2) == 1
+
+
+def test_the_healer_description():
+    l = Lancer()
+    w = Warrior()
+    h = Healer()
+    army1 = Army()
+    army2 = Army()
+    army1.append_units(l)
+    army2.append_units(w)
+    army2.append_units(h)
+    battle = Battle()
+
+    b = battle.fight_iter(army1, army2)
+
+    next(b)
+    assert w.health == 44
+    assert h.health == 57
+    assert l.health == 50
+
+    next(b)
+    assert w.health == 46
+    assert h.health == 57
+    assert l.health == 45
+
+
+def test_the_healer_max():
+    l = Lancer()
+    w = Warrior()
+    h = Healer()
+    army1 = Army()
+    army2 = Army()
+    army1.append_units(l)
+    army2.append_units(w)
+    army2.append_units(h)
+    b = Battle().fight_iter(army2, army1)
+    next(b)
+    assert w.health == w.HEALTH
+    assert h.health == h.HEALTH
+    assert l.health == 45
+
+
+def test_the_healer():
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+
+    # fight tests
+    chuck = Warrior()
+    bruce = Warrior()
+    carl = Knight()
+    dave = Warrior()
+    mark = Warrior()
+    bob = Defender()
+    mike = Knight()
+    rog = Warrior()
+    lancelot = Defender()
+    eric = Vampire()
+    adam = Vampire()
+    richard = Defender()
+    ogre = Warrior()
+    freelancer = Lancer()
+    vampire = Vampire()
+    priest = Healer()
+
+    assert fight(chuck, bruce) == True
+    assert fight(dave, carl) == False
+    assert chuck.is_alive == True
+    assert bruce.is_alive == False
+    assert carl.is_alive == True
+    assert dave.is_alive == False
+    assert fight(carl, mark) == False
+    assert carl.is_alive == False
+    assert fight(bob, mike) == False
+    assert fight(lancelot, rog) == True
+    assert fight(eric, richard) == False
+    assert fight(ogre, adam) == True
+    assert fight(freelancer, vampire) == True
+    assert freelancer.is_alive == True
+    assert freelancer.health == 14
+    priest.heal(freelancer)
+    assert freelancer.health == 16
+
+    # battle tests
+    my_army = Army()
+    my_army.add_units(Defender, 2)
+    my_army.add_units(Healer, 1)
+    my_army.add_units(Vampire, 2)
+    my_army.add_units(Lancer, 2)
+    my_army.add_units(Healer, 1)
+    my_army.add_units(Warrior, 1)
+
+    enemy_army = Army()
+    enemy_army.add_units(Warrior, 2)
+    enemy_army.add_units(Lancer, 4)
+    enemy_army.add_units(Healer, 1)
+    enemy_army.add_units(Defender, 2)
+    enemy_army.add_units(Vampire, 3)
+    enemy_army.add_units(Healer, 1)
+
+    assert Battle().fight(my_army, enemy_army) == False
+
+    army_3 = Army()
+    army_3.add_units(Warrior, 1)
+    army_3.add_units(Lancer, 1)
+    army_3.add_units(Healer, 1)
+    army_3.add_units(Defender, 2)
+
+    army_4 = Army()
+    army_4.add_units(Vampire, 3)
+    army_4.add_units(Warrior, 1)
+    army_4.add_units(Healer, 1)
+    army_4.add_units(Lancer, 2)
+
+    # REALLY True, but my code passed all checkio checks...
+    # assert Battle().fight(army_3, army_4) == True
+    assert Battle().fight(army_3, army_4) == False
