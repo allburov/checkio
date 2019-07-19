a6490e14dd0ab85d037b4f607136f16cdc6bbbdf
Aleksey Burov <allburov@gmail.com>
2019-07-19 11:46:58 +0700

    Power Supply, Lamp done with build-in iter()

--- GitHub/power_supply.py
+++ GitHub/power_supply.py
@@ -14,25 +14,27 @@
 # 
 # 
 # END_DESC
+from collections import defaultdict
+
 
 def power_supply(network, power_plants):
-    return set([])
+    network_graph = defaultdict(set)
+    # Parse
+    for f1, f2 in network:
+        network_graph[f1].add(f2)
+        network_graph[f2].add(f1)
 
-if __name__ == '__main__':
-    assert power_supply([['p1', 'c1'], ['c1', 'c2']], {'p1': 1}) == set(['c2']), 'one blackout'
-    assert power_supply([['c0', 'c1'], ['c1', 'p1'], ['c1', 'c3'], ['p1', 'c4']], {'p1': 1}) == set(['c0', 'c3']), 'two blackout'
-    assert power_supply([['p1', 'c1'], ['c1', 'c2'], ['c2', 'c3']], {'p1': 3}) == set([]), 'no blackout'
-    assert power_supply([['c0', 'p1'], ['p1', 'c2']], {'p1': 0}) == set(['c0', 'c2']), 'weak power-plant'
-    assert power_supply([['p0', 'c1'], ['p0', 'c2'], ['c2', 'c3'], ['c3', 'p4'], ['p4', 'c5']], {'p0': 1, 'p4': 1}) == set([]), 'cooperation'
-    assert power_supply([['c0', 'p1'], ['p1', 'c2'], ['c2', 'c3'], ['c2', 'c4'], ['c4', 'c5'],
-                         ['c5', 'c6'], ['c5', 'p7']],
-                        {'p1': 1, 'p7': 1}) == set(['c3', 'c4', 'c6']), 'complex cities 1'
-    assert power_supply([['p0', 'c1'], ['p0', 'c2'], ['p0', 'c3'],
-                         ['p0', 'c4'], ['c4', 'c9'], ['c4', 'c10'],
-                       ['c10', 'c11'], ['c11', 'p12'], ['c2', 'c5'],
-                       ['c2', 'c6'], ['c5', 'c7'], ['c5', 'p8']],
-                      {'p0': 1, 'p12': 4, 'p8': 1}) == set(['c6', 'c7']), 'complex cities 2'
-    assert power_supply([['c1', 'c2'], ['c2', 'c3']], {}) == set(['c1', 'c2', 'c3']), 'no power plants'
-    assert power_supply([['p1', 'c2'], ['p1', 'c4'], ['c4', 'c3'], ['c2', 'c3']], {'p1': 1}) == set(['c3']), 'circle'
-    assert power_supply([['p1', 'c2'], ['p1', 'c4'], ['c2', 'c3']], {'p1': 4}) == set([]), 'more than enough'
-    print("Looks like you know everything. It is time for 'Check'!")
+    visited = {x: False for x in network_graph.keys()}
+
+    def bfs(start, level):
+        visited[start] = True
+        if level == 0:
+            return
+        for neighbor in network_graph[start]:
+            bfs(neighbor, level - 1)
+
+    for plant, power in power_plants.items():
+        bfs(plant, power)
+
+    not_visited = set(x for x, visit in visited.items() if not visit)
+    return not_visited
