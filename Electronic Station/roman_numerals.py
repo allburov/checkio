385a560a025ecf7f04d6e36ac1fbbc0b8a0696ac
aburov <aburov@ptsecurity.com>
2019-12-29 14:02:28 +0700

    Add some task

--- Electronic Station/roman_numerals.py
+++ Electronic Station/roman_numerals.py
@@ -38,6 +38,12 @@ def first_solution(data: int) -> str:
     return repr
 
 
+def bisect_solution(data: int) -> str:
+    roman_char = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    # TODO:
+
+
 checkio = first_solution
 
 if __name__ == '__main__':
